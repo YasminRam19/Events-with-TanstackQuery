b5f80ceae6afec657c9c2b0284720ea3eb17608a
Yasmin Ramirez <yasmin.ramirez.sanchez19@gmail.com>
2023-12-26 20:30:08 -0600

    Enable Edit an Event button by fetching the editEvent request and add optimistic UI update to show the edits instantly without need to wait for the backend

--- src/components/Events/EditEvent.jsx
+++ src/components/Events/EditEvent.jsx
@@ -1,20 +1,86 @@
-import { Link, useNavigate } from 'react-router-dom';
-
-import Modal from '../UI/Modal.jsx';
-import EventForm from './EventForm.jsx';
+import { Link, useNavigate, useParams } from "react-router-dom";
+import Modal from "../UI/Modal.jsx";
+import EventForm from "./EventForm.jsx";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { fetchEvent, queryClient } from "../../util/http.js";
+import LoadingIndicator from "../UI/LoadingIndicator.jsx";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
+import { updateEvent } from "../../util/http.js";
 
 export default function EditEvent() {
   const navigate = useNavigate();
+  const params = useParams();
 
-  function handleSubmit(formData) {}
+  //Fetch existing data of the event
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ["events", params.id],
+    queryFn: ({ signal }) => fetchEvent({ signal, id: params.id }),
+  });
 
-  function handleClose() {
-    navigate('../');
+  //Fetch the edit event request
+  const { mutate } = useMutation({
+    mutationFn: updateEvent,
+    onMutate: async (data) => {
+      const newEvent = data.event;
+      //Make sure that if we had any outgoing queries for that key, those queries would be canceled, BUT will not cancel the mutation
+      await queryClient.cancelQueries({ queryKey: ["events", params.id] });
+
+      const previousEvent = queryClient.getQueryData(["events", params.id]);
+      //Setting our own data
+      queryClient.setQueryData(["events", params.id], newEvent);
+
+      //Need return the previousEvent as this will be the context if there is an error
+      return { previousEvent };
+    },
+    //Make sure that we can roll back to the old data if the mutation fail.
+    onError: (error, data, context) => {
+      queryClient.setQueryData(["events", params.id], context.previousEvent);
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries(["events", params.id]);
+    },
+  });
+
+  function handleSubmit(formData) {
+    mutate({ id: params.id, event: formData });
+    navigate("../"); //Go up one level to go to the page we are coming from, which is the event detail
   }
 
-  return (
-    <Modal onClose={handleClose}>
-      <EventForm inputData={null} onSubmit={handleSubmit}>
+  function handleClose() {
+    navigate("../");
+  }
+
+  let content;
+  if (isPending) {
+    content = (
+      <div className="center">
+        <LoadingIndicator />
+      </div>
+    );
+  }
+
+  if (isError) {
+    content = (
+      <>
+        <ErrorBlock
+          title="Failed to load event"
+          message={
+            error.info?.message ||
+            "Please check your inputs and try again later."
+          }
+        />
+        <div className="form-actions">
+          <Link to="../" className="button">
+            Okay
+          </Link>
+        </div>
+      </>
+    );
+  }
+
+  if (data) {
+    content = (
+      <EventForm inputData={data} onSubmit={handleSubmit}>
         <Link to="../" className="button-text">
           Cancel
         </Link>
@@ -22,6 +88,7 @@ export default function EditEvent() {
           Update
         </button>
       </EventForm>
-    </Modal>
-  );
+    );
+  }
+  return <Modal onClose={handleClose}>{content}</Modal>;
 }
